d1e913715291f58f3c043c67553b14687628e038
Rachelle <rrosiles@hawk.iit.edu>
2024-02-02 16:57:21 -0600

    Plotting works, documentation needed

---
+++ 2.2_subcansum/ChangingPlotParameters.swift
@@ -0,0 +1,8 @@
+//
+//  ChangingPlotParameters.swift
+//  2.2_subcansum
+//
+//  Created by PHYS 440 Rachelle on 2/2/24.
+//
+
+import Foundation
---
+++ 2.2_subcansum/PlotClass.swift
@@ -0,0 +1,8 @@
+//
+//  PlotClass.swift
+//  2.2_subcansum
+//
+//  Created by PHYS 440 Rachelle on 2/2/24.
+//
+
+import Foundation
